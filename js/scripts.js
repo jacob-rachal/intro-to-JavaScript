7d7c3550b1bcbec7a4e5521e494ef84cfee0cab5
Jacob Rachal <jacob_rachal@mail.fresnostate.edu>
2018-08-15 18:55:00 -0700

    Start of day 2 branch, 8/15/2018

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 // console.log("Hello World!");
-// Jacob Rachal 8/13/2018
+// Jacob Rachal 8/15/2018
 //JavaScript is Object-Oriented-Programming.
 console.log("Hello World!");
 //access the console, go down a level, this is the action we want to do (or go down another level), passing our argument within parenthesees, passing along a string that says Hello World!.
