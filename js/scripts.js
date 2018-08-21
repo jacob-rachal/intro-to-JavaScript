460c05021092790f7ed18825b6068940cb89ed68
Jacob Rachal <jacob_rachal@mail.fresnostate.edu>
2018-08-20 17:55:11 -0700

    Finished up the first day's takehome, slightly late.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
-// console.log("Hello World!");
 // Jacob Rachal 8/13/2018
-//JavaScript is Object-Oriented-Programming.
 console.log("Hello World!");
-//access the console, go down a level, this is the action we want to do (or go down another level), passing our argument within parenthesees, passing along a string that says Hello World!.
+console.log("My name is Jacob Rachal, and I am a recently-graduated college student with a Bachelor's degree in Computer Science.")
+console.log("Unfortunately, I've been having trouble finding a job.");
+console.log("But while I am job-hunting, I thought I'd take these Geekwise classes and learn all the stuff that my college professors thought I didn't need to know. (Or didn't have time to teach.)");
+console.log("It is my hope that I will be able to get out of this class a solid understanding of how to make use of JavaScript in my future projects, while also gaining experience experimenting with it.");
+console.log("As for my hobbies; I really like to read books and online stories that of the genres sci-fi and fantasy, I enjoy seeing what I can construct with LEGOs, and watching videos that make me pleased to be a part of the human race");
