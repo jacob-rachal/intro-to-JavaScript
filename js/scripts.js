8aa28088b7856e5f6fb56ec3bb652a9d619b44af
Jacob Rachal <jacob_rachal@mail.fresnostate.edu>
2018-08-22 15:49:53 -0700

    I think I'm done with the day 2 takehome? Maybe? All I have left is merging it with gh-pages I believe.

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,22 @@
-// Jacob Rachal 8/15/2018
-// Day 2 - variable and conslole.log.
-
-
-
-// Day 2 - MATH!!! ==========================================
-// Gets a random number.  Using JavaScript objects.
-// const rand = Math.random(); //get a random # from 0 to 1, so a decimal with lot's of decimal places.
-
-// pseudo- GIVES us a random number, then multiply it by ten.
-// Next add 1 so we cannot get 0. Finally, round down the number.
-// const rand = Math.floor( (Math.random()*10) +1 ); //floor rounds down, the +1 rounds us up so we are above 0.
-// const rand = Math.floor( ((Math.random()*10) *10)+1 ); // returns a random # between 0 and 100.
-// console.log(rand);
+// Jacob Rachal 8/22/2018
+// Day 2 - variable and conslole.log and MATH.
 
 // Here are some other ways to notify the user
 // Simple popup.
 //alert(rand); //makes a popup when the page is loaded/reloaded.
-
 // User clicks cancel or OK
 //confirm(rand); //there is a way to get the user's response back from this.
-
 //Accepts a user's input
 //prompt(rand); //for input, you can get back the user's input.
 // Accepts a user's input then alerts the name.
-let name = prompt('Please enter your name:');
-alert(name); //for day 2 takehome.
 
-// Function definition
+//for day 2 takehome.
+let firstName = prompt('Please enter your first name:');
+let lastName = prompt('Thanks! Now please enter your last name:');
+console.log("The user's first name is " + firstName);
+alert("The user's last name is: " + lastName);
+
+let userBDay = prompt("Excellent! One more question for you. What's your birthday in (mm/dd/yy) format? ");
+confirm(" Is " + userBDay + " really your birthday? (Click OK for yes and CANCEL for no.)" ); //is there a way to catch what the user clicks on?
+alert(firstName + " " + lastName +"'s birthday is on " + userBDay);
+//...wait, was that it?
