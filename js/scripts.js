2060d00d4e4e2ff0c319315436dc05db7a67a744
Jacob Rachal <jacob_rachal@mail.fresnostate.edu>
2018-08-27 17:29:43 -0700

    Including the stuff from the day 2 takehome in order to allow this to merge with gh-pages easier. Hopefully.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,32 @@
 // Jacob Rachal 8/22/2018
+// Day 2 - variable and conslole.log and MATH.
+
+// Here are some other ways to notify the user
+// Simple popup.
+//alert(rand); //makes a popup when the page is loaded/reloaded.
+// User clicks cancel or OK
+//confirm(rand); //there is a way to get the user's response back from this. (TAKE AS A BOOLEAN)
+//Accepts a user's input
+//prompt(rand); //for input, you can get back the user's input.
+// Accepts a user's input then alerts the name.
+
+//for day 2 takehome.
+// let firstName = prompt('Please enter your first name:');
+// let lastName = prompt('Thanks! Now please enter your last name:');
+// console.log("The user's first name is " + firstName);
+// alert("The user's last name is: " + lastName);
+//
+// let userBDay = prompt("Excellent! One more question for you. What's your birthday in (mm/dd/yy) format? ");
+// let dodad = confirm(" Is " + userBDay + " really your birthday? (Click OK for yes and CANCEL for no.)" );
+// //Hang on, let me see if this works...
+// if (dodad) {
+//   console.log(dodad);
+//   alert(firstName + " " + lastName +"'s birthday is on " + userBDay);
+// } else {
+//   console.log(dodad);
+//   alert(firstName + " " + lastName +"'s birthday is NOT on " + userBDay);
+}// Wow, it does work.
+
 // Day 3 - takehome
 // Day 3 -- Switch statements.
 
